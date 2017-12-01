83d1fdf0923e344e95ebc6162a5266e6352375ed
Edvard <edvardlindelof@gmail.com>
2017-12-01 17:30:16 +0100

    some StructLogic methods and testing

--- src/main/scala/sp/domain/logic/StructLogic.scala
+++ src/main/scala/sp/domain/logic/StructLogic.scala
@@ -5,32 +5,35 @@ import sp.domain._
 object StructLogic extends StructLogics
 
 trait StructLogics {
-  // TODO: need some more test before released
 
   implicit class StructExtras(x: Struct) {
-    def getChildren(node: StructNode): List[StructNode] = {
-      x.items.filter(_.parent.contains(node.nodeID)).toList
+    def getChildren(nodeID: ID): Set[StructNode] = {
+      x.items.filter(_.parent.contains(nodeID))
     }
 
-    def getChildrenMap: Map[ID, List[StructNode]] = {
-      x.items.foldLeft(Map[ID, List[StructNode]]()){(a, b) =>
-        val parentChildren = b.parent.map(p => b :: a.getOrElse(p, List()))
+    def getChildrenMap: Map[ID, Set[StructNode]] = {
+      x.items.foldLeft(Map[ID, Set[StructNode]]()) { (a, b) =>
+        val parentChildren = b.parent.map(p => a.getOrElse(p, Set()) + b)
         parentChildren.map(ch => a + (b.parent.get -> ch)).getOrElse(a)
       }
     }
 
-    def getAllChildren(node: StructNode): List[StructNode] = {
-      def rec(currentNode: StructNode, aggr: Set[StructNode]): Set[StructNode] = {
-        if (aggr.contains(currentNode)) aggr
-        else {
-          val direct = getChildren(currentNode).toSet
-          val updA = aggr ++ direct
-          direct.flatMap(n => rec(n, updA))
-        }
+    def getAllChildren(nodeID: ID): Set[StructNode] = {
+      def rec(currentNodeID: ID, aggr: Set[StructNode]): Set[StructNode] = {
+        val directChildren = getChildren(currentNodeID)
+        if (directChildren.isEmpty) aggr
+        else directChildren.flatMap(c => rec(c.nodeID, aggr ++ directChildren))
       }
-      rec(node, Set()).toList
+      rec(nodeID, Set())
     }
 
+    /*
+    def getNodeAndChildren(nodeID: ID): Set[StructNode] = {
+      x.items.find(_.nodeID == nodeID).map(n => getAllChildren(nodeID) + n).getOrElse(Set())
+    }
+    */
+
+    // TODO completely untested
     def getItemNodeMap: Map[ID, Set[StructNode]] = {
       x.items.foldLeft(Map[ID, Set[StructNode]]()) { (a, b) =>
         val m = a.getOrElse(b.item, Set()) + b
@@ -63,7 +66,13 @@ trait StructLogics {
       x.copy(items = filtered)
     }
 
-    def hasLoops = {
+    def --(nodeIDs: Set[ID]) = {
+      val filtered = x.items.filterNot(n => nodeIDs.contains(n.nodeID))
+      x.copy(items = filtered)
+    }
+
+
+    def hasLoop = {
       def req(currentNode: StructNode, aggr: Set[ID]): Boolean = {
         currentNode.parent match {
           case None => true
@@ -73,16 +82,41 @@ trait StructLogics {
             req(p, aggr + currentNode.nodeID)
         }
       }
-      x.items.forall(s => req(s, Set()))
+      !x.items.forall(s => req(s, Set()))
     }
 
     def +(node: StructNode) = {
       x.copy(items = x.items + node)
     }
 
-    def ++(xs: List[StructNode]) = {
-      x.copy(items = x.items ++ xs)
+    def ++(xs: Set[StructNode]) = {
+      // map needed to get the parents right
+      val oldToNewID = xs.foldLeft(Map[ID, ID]())((m, n) => m + (n.nodeID -> ID.newID))
+      val newXs = xs.map(n => n.copy(parent = n.parent.map(oldToNewID), nodeID = oldToNewID(n.nodeID)))
+      x.copy(items = x.items ++ newXs)
     }
+
+    def addTo(parentNodeID: ID, xs: Set[StructNode]) = {
+      // map needed to get the parents right
+      val oldToNewID = xs.foldLeft(Map[ID, ID]())((m, n) => m + (n.nodeID -> ID.newID))
+      val newXs = xs.map { n =>
+        if (n.parent.isEmpty) n.copy(parent = Some(parentNodeID), nodeID = oldToNewID(n.nodeID))
+        else n.copy(parent = Some(oldToNewID(n.parent.get)), nodeID = oldToNewID(n.nodeID))
+      }
+      x.copy(items = x.items ++ newXs)
+    }
+
+    def moveNode(movedNodeID: ID, receivingNodeID: ID) = {
+      val movedNode = x.items.find(_.nodeID == movedNodeID)
+      val receivingNode = x.items.find(_.nodeID == receivingNodeID)
+      val maybeNewStruct = movedNode.zip(receivingNode).headOption.flatMap { case (mov, rec) =>
+        val nodesToMove = getAllChildren(mov.nodeID) + mov.copy(parent = None)
+        val newStruct = (x -- nodesToMove.map(_.nodeID)).addTo(rec.nodeID, nodesToMove)
+        if (newStruct.hasLoop) None else Some(newStruct)
+      }
+      maybeNewStruct.getOrElse(x)
+    }
+
 
   }
 
@@ -109,7 +143,6 @@ trait StructLogics {
     }
     digger(xs.toList, None)
   }
-
 
 }
 
--- src/test/scala/sp/domain/logic/StructLogicsTest.scala
+++ src/test/scala/sp/domain/logic/StructLogicsTest.scala
@@ -31,6 +31,7 @@ class StructLogicsTest extends FreeSpec {
   val snTest = Struct("test", Set(sn1, sn2.copy(parent = Some(sn1.nodeID)), sn3))
 
   "Methods tests" - {
+
     "making a struct using the DSL" in {
       val simpleStruct = Struct("test", makeStructNodes(
         o1.children(o2), o3
@@ -41,9 +42,60 @@ class StructLogicsTest extends FreeSpec {
       )
       val s = Struct("hej", aStruct)
       s.printNiceTree(list)
-
     }
 
+    val struct = Struct(
+      "test",
+      makeStructNodes(
+        t1.children(
+          o1,
+          o2.children(
+            sop1
+          )
+        ),
+        t2.children(
+          t3
+        )
+      )
+    )
+    val t1NodeID = struct.items.find(_.item == t1.id).get.nodeID
+    val t2NodeID = struct.items.find(_.item == t2.id).get.nodeID
+    val t3NodeID = struct.items.find(_.item == t3.id).get.nodeID
+    val o2NodeID = struct.items.find(_.item == o2.id).get.nodeID
+    val sop1NodeID = struct.items.find(_.item == sop1.id).get.nodeID
+
+    "getChildren(t1-nodeID) should return nodes of o1 and o2" in {
+      val allChildrenItemIDs = struct.getChildren(t1NodeID).map(_.item)
+      assert(allChildrenItemIDs == Set(o1, o2).map(_.id))
+    }
+    "getAllChildren(t1-nodeID) should return nodes of o1, o2 and sop1" in {
+      val allChildrenItemIDs = struct.getAllChildren(t1NodeID).map(_.item)
+      assert(allChildrenItemIDs == Set(o1, o2, sop1).map(_.id))
+    }
+
+    "addTo(t2-nodeID, t4.children(sop2)) should make getAllChildren(t2-nodeID) return nodes of t3, t4 and sop2" in {
+      val structAfterAdd = struct.addTo(t2NodeID, makeStructNodes(t4.children(sop2)))
+      val allChildrenItemIDs = structAfterAdd.getAllChildren(t2NodeID).map(_.item)
+      assert(allChildrenItemIDs == Set(t3, t4, sop2).map(_.id))
+    }
+
+    "hasLoops should return false, but return true if sop1-node is made parent of t1-node" in {
+      val structWithLoop = struct.copy(
+        items = struct.items.map { n =>
+          if (n.nodeID == t1NodeID) n.copy(parent = Some(sop1NodeID)) else n
+        }
+      )
+      assert(struct.hasLoop == false)
+      assert(structWithLoop.hasLoop == true)
+    }
+
+    "after moveNode(o2-nodeID, t3-nodeID) t2-node should have t3, o2 and sop1, and t1-node just o1" in {
+      val structAfterMove = struct.moveNode(o2NodeID, t3NodeID)
+      val allChildrenItemIDsOft2 = structAfterMove.getAllChildren(t2NodeID).map(_.item)
+      assert(allChildrenItemIDsOft2 == Set(t3, o2, sop1).map(_.id))
+      val allChildrenItemIDsOft1 = structAfterMove.getAllChildren(t1NodeID).map(_.item)
+      assert(allChildrenItemIDsOft1 == Set(o1.id))
+    }
 
   }
 }
